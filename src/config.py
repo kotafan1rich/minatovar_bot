3b10de4826abe444bf7e894cb7217bbe0c865e81
kotafan1rich <lesapond2007@gmail.com>
2024-11-08 20:20:46 +0300

    Changes to be committed: 	modified:   .env 	modified:   docker-compose.yaml 	modified:   src/config.py 	modified:   src/create_bot.py

--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@ TOKEN = os.getenv("TOKEN") if os.getenv("DEBUG") == 'False' else os.getenv("TEST
 REDIS_PORT = os.getenv("REDIS_PORT")
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_DB = os.getenv("REDIS_DB")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+REDIS_USER = os.getenv("REDIS_USER")
--- src/create_bot.py
+++ src/create_bot.py
@@ -2,12 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.redis import RedisStorage
 
-from config import REDIS_HOST, REDIS_DB, REDIS_PORT, TOKEN
+from config import REDIS_HOST, REDIS_DB, REDIS_PORT, TOKEN, REDIS_PASSWORD, REDIS_USER
 
 import redis
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
-REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD, username=REDIS_USER)
+REDIS_URL = f"redis://{REDIS_USER}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
 storage = RedisStorage.from_url(REDIS_URL)
 
