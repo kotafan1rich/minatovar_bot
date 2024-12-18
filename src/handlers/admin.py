1abcdbcea4fd6f4bd623540ba10270031d7590eb
kotafan1rich <lesapond2007@gmail.com>
2024-12-18 22:59:39 +0300

    Changes to be committed: 	modified:   src/handlers/admin.py 	modified:   src/handlers/messages.py

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -1,6 +1,6 @@
-import logging
 from aiogram import Dispatcher, types
 from aiogram.filters import Command
+from aiogram import F
 
 from db.dals import DataDAL
 
@@ -12,43 +12,34 @@ admins = [1019030670, 1324716819]
 
 
 async def change_shoes_price(message: types.Message):
-    if message.from_user.id in admins:
-        try:
-            price = await DataDAL().set_shoes_price(
-                price=int(message.text.split(" ")[1])
-            )
+    try:
+        price = await DataDAL().set_shoes_price(price=int(message.text.split(" ")[1]))
 
-            await bot.send_message(message.from_user.id, str(price))
-        except IndexError:
-            await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
-        except ValueError:
-            await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
+        await bot.send_message(message.from_user.id, str(price))
+    except IndexError:
+        await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
+    except ValueError:
+        await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
 
 
 async def change_cloth_price(message: types.Message):
-    if message.from_user.id in admins:
-        try:
-            price = await DataDAL().set_cloth_price(
-                price=int(message.text.split(" ")[1])
-            )
-            await bot.send_message(message.from_user.id, str(price))
-        except IndexError:
-            await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
-        except ValueError:
-            await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
+    try:
+        price = await DataDAL().set_cloth_price(price=int(message.text.split(" ")[1]))
+        await bot.send_message(message.from_user.id, str(price))
+    except IndexError:
+        await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
+    except ValueError:
+        await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
 
 
 async def change_current_rate(message: types.Message):
-    if message.from_user.id in admins:
-        try:
-            rate = await DataDAL().set_current_rate(
-                rate=float(message.text.split(" ")[1])
-            )
-            await bot.send_message(message.from_user.id, str(rate))
-        except IndexError:
-            await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
-        except ValueError:
-            await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
+    try:
+        rate = await DataDAL().set_current_rate(rate=float(message.text.split(" ")[1]))
+        await bot.send_message(message.from_user.id, str(rate))
+    except IndexError:
+        await bot.send_message(message.from_user.id, NON_ARGUMENT_ERROR)
+    except ValueError:
+        await bot.send_message(message.from_user.id, BAD_FORMAT_ERROR)
 
 
 async def admin_help(message: types.Message):
@@ -57,7 +48,13 @@ async def admin_help(message: types.Message):
 
 
 def register_handlers_admin(dp: Dispatcher):
-    dp.message.register(change_shoes_price, Command("shoes"))
-    dp.message.register(change_cloth_price, Command("cloth"))
-    dp.message.register(change_current_rate, Command("rate"))
-    dp.message.register(admin_help, Command("adminhelp"))
+    dp.message.register(
+        change_shoes_price, Command("shoes"), F.from_user.id.in_(admins)
+    )
+    dp.message.register(
+        change_cloth_price, Command("cloth"), F.from_user.id.in_(admins)
+    )
+    dp.message.register(
+        change_current_rate, Command("rate"), F.from_user.id.in_(admins)
+    )
+    dp.message.register(admin_help, Command("adminhelp"), F.from_user.id.in_(admins))
