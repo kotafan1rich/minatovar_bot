b81c76d49689eb1d03ed1cc2c3cc0ea4f09654a9
kotafan1rich <lesapond2007@gmail.com>
2025-01-06 13:38:53 +0300

    config create_bot main; add wait_postgres

--- minatovar_bot/src/create_bot.py
+++ minatovar_bot/src/create_bot.py
@@ -1,5 +1,5 @@
+import logging
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.redis import RedisStorage
 
 from config import TOKEN
@@ -8,5 +8,8 @@ from db.connection import REDIS_URL
 
 storage = RedisStorage.from_url(REDIS_URL)
 
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=storage)
--- minatovar_bot/src/main.py
+++ minatovar_bot/src/main.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python
 # vim: set fileencoding=UTF-8
 import asyncio
-import logging
-import os
-
-from config import DATA_FILE
+from aiogram.types import BotCommand
 from create_bot import bot, dp
-from handlers import client, admin
-from db.dals import DataDAL
-
-logging.basicConfig(level=logging.INFO)
-
-client.register_handlers_client(dp)
-admin.register_handlers_admin(dp)
+from db.dals import SettingsDAL
+from handlers import admin_router, client_router, order_roter
+from db.session import async_session
+from middlewares.middleware import (
+    DBSessionMiddleware,
+    StartMiddleware,
+    CallbackDataMiddleware,
+)
 
 
 async def on_startapp():
-    default_data = {"current_rate": None, "cloth_price": None, "shoes_price": None}
-    if not os.path.exists(DATA_FILE):
-        await DataDAL().save_data(default_data)
-    else:
-        data = await DataDAL().load_data()
-        if not data:
-            DataDAL().save_data(default_data)
+    await bot.set_my_commands([BotCommand(command="/start", description="Начать")])
+    async with async_session() as session:
+        settings_dal = SettingsDAL(session)
+        if not await settings_dal.param_exists("current_rate"):
+            await settings_dal.set_param("current_rate", 0.0)
+        if not await settings_dal.param_exists("shoes_price"):
+            await settings_dal.set_param("shoes_price", 0.0)
+        if not await settings_dal.param_exists("cloth_price"):
+            await settings_dal.set_param("cloth_price", 0.0)
 
 
 async def main():
-    await on_startapp()
+    await dp.emit_startup(await on_startapp())
     await bot.delete_webhook()
+    dp.update.outer_middleware(DBSessionMiddleware())
+    admin_router.callback_query.middleware.register(CallbackDataMiddleware())
+    order_roter.callback_query.middleware.register(CallbackDataMiddleware())
+    client_router.message.middleware.register(StartMiddleware())
+    client_router.callback_query.middleware.register(CallbackDataMiddleware())
+    dp.include_router(client_router)
+    dp.include_router(order_roter)
+    dp.include_router(admin_router)
     await dp.start_polling(bot)
 
 
