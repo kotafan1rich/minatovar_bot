2b4d61aa8d748cec5866805f88aeb795815d16d4
kotafan1rich <lesapond2007@gmail.com>
2025-01-16 00:10:33 +0300

    backup auto

--- postgres/create_backup.sh
+++ postgres/create_backup.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 # Параметры
-CONTAINER_NAME="postgres"          # Название контейнера из docker-compose.yml
-BACKUP_DIR="./postgres/backups"             # Папка для хранения бэкапов
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S") # Метка времени
+BACKUP_DIR="/backups"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="$BACKUP_DIR/db_backup_$TIMESTAMP.sql"
 
-# Данные для подключения
-POSTGRES_USER="postgres"           # Имя пользователя PostgreSQL
-POSTGRES_DB="min_db"               # Имя базы данных
-POSTGRES_PASSWORD="2007Fj2007"     # Пароль пользователя
+# Подключение
+POSTGRES_USER="postgres"
+POSTGRES_PASSWORD="2007Fj2007"
+POSTGRES_DB="min_db"
+POSTGRES_HOST="postgres"
 
-# Проверяем, существует ли папка для бэкапов, если нет — создаем
+# Проверяем, существует ли папка для бэкапов
 if [ ! -d "$BACKUP_DIR" ]; then
   mkdir -p "$BACKUP_DIR"
 fi
 
-# Выполняем команду создания бэкапа с использованием пароля
-docker exec -e PGPASSWORD=$POSTGRES_PASSWORD -t $CONTAINER_NAME pg_dump -U $POSTGRES_USER -d $POSTGRES_DB > $BACKUP_FILE
+# Создание бэкапа
+export PGPASSWORD=$POSTGRES_PASSWORD
+pg_dump -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > "$BACKUP_FILE"
 
-# Проверяем успешность создания бэкапа
 if [ $? -eq 0 ]; then
   echo "Бэкап успешно сохранен: $BACKUP_FILE"
 else
   echo "Ошибка создания бэкапа"
-  # Удаляем файл, если он пустой
   [ -e "$BACKUP_FILE" ] && rm "$BACKUP_FILE"
 fi
+
+# Удаляем все, кроме двух последних бэкапов
+ls -tp "$BACKUP_DIR" | grep -v '/$' | tail -n +3 | xargs -I {} rm -- "$BACKUP_DIR/{}"
