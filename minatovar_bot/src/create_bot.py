21f652f5871ddce5d82577d62459dab7af71e822
kotafan1rich <lesapond2007@gmail.com>
2025-01-25 12:40:27 +0300

    parse mode html

--- minatovar_bot/src/create_bot.py
+++ minatovar_bot/src/create_bot.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.client.default import DefaultBotProperties
 
 from config import TOKEN
 from db.connection import REDIS_URL
@@ -15,5 +16,5 @@ logger = logging.getLogger(__name__)
 
 admins = [1019030670, 1324716819, 1423930901]
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 dp = Dispatcher(storage=storage)
