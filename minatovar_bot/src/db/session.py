65b6988fb6c7d4fc0299cd0b04b77b3032069600
Aleksis <lesapond2007@gmail.com>
2025-08-20 16:30:03 +0300

    change db_echo param

--- minatovar_bot/src/db/session.py
+++ minatovar_bot/src/db/session.py
@@ -2,9 +2,9 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from src.config import get_postgres_url
+from src.config import get_postgres_url, settings
 
-engine = create_async_engine(get_postgres_url(), future=True, echo=True)
+engine = create_async_engine(url=get_postgres_url(), future=True, echo=settings.DEBUG)
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
